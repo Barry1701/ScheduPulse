5ef50da8ced6e1bb93d9e7fe70f84141e14e9505
Bartosz Stanczuk <synergy170185@gmail.com>
2024-08-08 16:03:34 +0000

    Final updates

---
+++ meetings/test_forms.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from meetings.forms import MeetingForm
+from meetings.models import Room
+from datetime import date, time, timedelta
+
+class TestMeetingForm(TestCase):
+
+    def setUp(self):
+        self.room = Room.objects.create(name="Room 1", floor=1, room_number=101)
+
+    def test_form_is_valid(self):
+        future_date = date.today() + timedelta(days=1)  # Używamy daty przyszłej
+        form = MeetingForm({
+            'title': 'Test Meeting',
+            'date': future_date,
+            'start_time': time(9, 0),
+            'duration': 2,
+            'room': self.room.id  # używamy identyfikatora pokoju
+        })
+        self.assertTrue(form.is_valid(), msg="Form is invalid")
+
+    def test_form_is_invalid(self):
+        form = MeetingForm({
+            'title': '',
+            'date': '',
+            'start_time': '',
+            'duration': '',
+            'room': ''
+        })
+        self.assertFalse(form.is_valid(), msg="Form is valid")
---
+++ meetings/test_views.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.test import TestCase
+from meetings.models import Room, Meeting
+from datetime import datetime, timedelta
+
+class TestMeetingViews(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser",
+            password="password"
+        )
+        self.room = Room.objects.create(name="Room 1", floor=1, room_number=101)
+        future_date = datetime.now().date() + timedelta(days=1)  # Używamy daty przyszłej
+        self.meeting = Meeting.objects.create(
+            title="Test Meeting",
+            description="Test Description",
+            date=future_date,
+            start_time=datetime.now().time(),
+            duration=1,
+            room=self.room,
+            created_by=self.user
+        )
+
+    def test_meeting_create_view(self):
+        self.client.login(username="testuser", password="password")
+        future_date = datetime.now().date() + timedelta(days=1)  # Używamy daty przyszłej
+        response = self.client.post(reverse('new'), {
+            'title': 'New Meeting',
+            'date': future_date,
+            'start_time': '10:00',
+            'duration': 1,
+            'room': self.room.id  # używamy identyfikatora pokoju
+        })
+        if response.status_code != 302:
+            print(response.content)
+        self.assertEqual(response.status_code, 302)
+
+    def test_meeting_edit_view(self):
+        self.client.login(username="testuser", password="password")
+        future_date = datetime.now().date() + timedelta(days=1)  # Używamy daty przyszłej
+        response = self.client.post(reverse('edit_meeting', args=[self.meeting.id]), {
+            'title': 'Updated Meeting',
+            'date': future_date,
+            'start_time': '11:00',
+            'duration': 2,
+            'room': self.room.id  # używamy identyfikatora pokoju
+        })
+        if response.status_code != 302:
+            print(response.content)
+        self.assertEqual(response.status_code, 302)
